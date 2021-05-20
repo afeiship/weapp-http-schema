af39de684c70bbde10c281e847a68f2633fc3ced
afeiship <1290657123@qq.com>
2021-05-20 20:34:52 +0800

    fix: optional options

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import httpSchema from '@jswork/http-rest-config';
 import NxTaroRequest from '@jswork/next-taro-request';
 
-export default (inConfig, inOptions): any => {
+export default (inConfig, inOptions?): any => {
   const context = {};
   const http = NxTaroRequest.getInstance(inOptions);
   httpSchema(context, http, inConfig);
