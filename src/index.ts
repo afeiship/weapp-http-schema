36f87c2a24f43481aab72b9e1b44309fb026797f
afeiship <1290657123@qq.com>
2021-05-21 11:36:04 +0800

    refactor: use nomalize implment

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import httpSchema from '@jswork/http-rest-config';
-import NxTaroRequest from '@jswork/next-taro-request';
+import NxFetch from '@jswork/next-fetch';
+import httpSchema from '@jswork/http-schema';
+import nxTaroRequest from '@jswork/next-taro-request';
+import nxFetchWithContentType from '@jswork/next-fetch-with-content-type';
+import nxFetchWithDebug from '@jswork/next-fetch-with-debug';
+import nxApplyFetchMilddeware from '@jswork/next-apply-fetch-middleware';
 
 export default (inConfig, inOptions?): any => {
-  const context = {};
-  const http = NxTaroRequest.getInstance(inOptions);
-  httpSchema(context, http, inConfig);
-  return context;
+  const fetch = nxApplyFetchMilddeware([nxFetchWithContentType, nxFetchWithDebug], nxTaroRequest);
+  const http = NxFetch.getInstance({
+    pipeStyle: 'request',
+    fetch,
+    ...inOptions
+  });
+  return httpSchema(inConfig, http);
 };
