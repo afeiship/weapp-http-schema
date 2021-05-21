24e390e2b672d998fda193e4e5d52a4a2bba0c95
afeiship <1290657123@qq.com>
2021-05-21 12:54:39 +0800

    chore: remove content-type

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import NxFetch from '@jswork/next-fetch';
 import httpSchema from '@jswork/http-schema';
 import nxTaroRequest from '@jswork/next-taro-request';
-import nxFetchWithContentType from '@jswork/next-fetch-with-content-type';
 import nxFetchWithDebug from '@jswork/next-fetch-with-debug';
 import nxApplyFetchMilddeware from '@jswork/next-apply-fetch-middleware';
 
 export default (inConfig, inOptions?): any => {
-  const fetch = nxApplyFetchMilddeware([nxFetchWithContentType, nxFetchWithDebug])(nxTaroRequest);
+  const fetch = nxApplyFetchMilddeware([nxFetchWithDebug])(nxTaroRequest);
   const http = NxFetch.getInstance({
     pipeStyle: 'request',
     fetch,
